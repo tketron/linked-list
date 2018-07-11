594af5f3716bd262cab96e4e2c6048ef21b4226f
jxhdev <jxhdev@gmail.com>
2018-07-11 14:47:41 -0700

    updating seed file

--- seed.sql
+++ seed.sql
@@ -1,14 +1,15 @@
-DROP DATABASE IF EXISTS "jobs_db";
-CREATE DATABASE "jobs_db";
-\c "jobs_db";
+DROP DATABASE IF EXISTS "linked_list_db";
+CREATE DATABASE "linked_list_db";
+\c "linked_list_db";
 
-CREATE TABLE companies (id SERIAL PRIMARY KEY, handle TEXT UNIQUE NOT NULL, password TEXT NOT NULL, name TEXT, logo TEXT);
-INSERT INTO companies (name, logo) VALUES ('haleyaldrich', 'environment', 'Haley & Aldrich', 'http://www.haleyandaldrich.com');
+CREATE TABLE companies (id SERIAL PRIMARY KEY, handle TEXT UNIQUE NOT NULL, password TEXT NOT NULL, name TEXT NOT NULL, logo TEXT, email TEXT NOT NULL UNIQUE);
+INSERT INTO companies (handle, password, name, logo, email) VALUES ('haleyaldrich', 'environment', 'Haley & Aldrich', 'http://www.haleyandaldrich.com', 'hr@haleyaldrich.com');
 
-CREATE TABLE jobs (id SERIAL PRIMARY KEY, title TEXT, salary INTEGER, equity REAL, company_id INTEGER REFERENCES companies (id) ON DELETE CASCADE);
-INSERT INTO jobs (title, salary, equity, company_id) VALUES ('Teacher', '100000', '4', 1);
+CREATE TABLE jobs (id SERIAL PRIMARY KEY, title TEXT, salary INTEGER, equity REAL, company INTEGER REFERENCES companies (id) ON DELETE CASCADE);
+INSERT INTO jobs (title, salary, equity, company) VALUES ('Teacher', '100000', '4', 1);
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, first_name TEXT, last_name TEXT, email TEXT, photo TEXT, current_company_id INTEGER REFERENCES companies (id) ON DELETE CASCADE);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL, photo TEXT, current_company TEXT REFERENCES companies (handle) ON DELETE CASCADE);
+
 INSERT INTO users (username, password, first_name, last_name, email, photo) VALUES ('tketron', 'abc123', 'Tyler', 'Ketron', 'tketron@gmailcom', 'http//www.fakeavatar.com');
 
 CREATE TABLE jobs_users (id SERIAL PRIMARY KEY, job_id INTEGER REFERENCES jobs (id) ON DELETE CASCADE, company_id INTEGER REFERENCES companies (id) ON DELETE CASCADE);
